095cacf6b8927e11908c46d047ceeaaebd59aa7e
Miguel Rugerio <mrugerio@gmail.com>
2023-01-18 16:58:39 -0600

    Garbage Collector

---
+++ GarbageCollector/src/com/curso/Scope.java
@@ -0,0 +1,16 @@
+package com.curso;
+
+public class Scope {
+
+	public static void main(String[] args) {
+
+		String one, two;
+		one = new String("a");
+		two = new String("b");
+		one = two;
+		String three = one;
+		one = null;
+		
+	}
+
+}
