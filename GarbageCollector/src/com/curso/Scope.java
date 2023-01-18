13d92b207102c38cf777824f9770fb8d8a476b65
Miguel Rugerio <mrugerio@gmail.com>
2023-01-18 17:10:19 -0600

    GC

--- GarbageCollector/src/com/curso/Scope.java
+++ GarbageCollector/src/com/curso/Scope.java
@@ -5,12 +5,16 @@ public class Scope {
 	public static void main(String[] args) {
 
 		String one, two;
+		//Objetos = 0;
 		one = new String("a");
 		two = new String("b");
+		//Objetos = 2;
 		one = two;
+		//Objetos = // NO SABEMOS //1 ó 2
 		String three = one;
+		//Objetos = // NO SABEMOS //1 ó 2
 		one = null;
-		
+		//Esperaria solo tener 1 Objeto
 	}
 
 }
