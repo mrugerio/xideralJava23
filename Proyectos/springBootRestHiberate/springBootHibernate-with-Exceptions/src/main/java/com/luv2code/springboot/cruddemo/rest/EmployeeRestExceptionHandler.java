29f66870d4a329c3c3adadfc106b1ef49cce540a
Miguel Rugerio <mrugerio@gmail.com>
2023-02-06 16:25:00 -0600

    SpringBootRest with Handle Exceptions

---
+++ Proyectos/springBootRestHiberate/springBootHibernate-with-Exceptions/src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.luv2code.springboot.cruddemo.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class EmployeeRestExceptionHandler {
+	
+	@ExceptionHandler
+	public ResponseEntity<EmployeeErrorResponse> handleException(EmployeeNotFoundException exc) {
+		
+		
+		EmployeeErrorResponse error = new EmployeeErrorResponse(
+											HttpStatus.NOT_FOUND.value(),
+											exc.getMessage(),
+											System.currentTimeMillis());
+		
+		// return ResponseEntity
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	}
+	
+	
+
+	@ExceptionHandler
+	public ResponseEntity<EmployeeErrorResponse> handleException(Exception exc) {
+		
+		// create CustomerErrorResponse
+		
+		EmployeeErrorResponse error = new EmployeeErrorResponse(
+											HttpStatus.BAD_REQUEST.value(),
+											exc.getMessage(),
+											System.currentTimeMillis());
+		
+		// return ResponseEntity
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}
+	
+}
+
+
+
+
+
